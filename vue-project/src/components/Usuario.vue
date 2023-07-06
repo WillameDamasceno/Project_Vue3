ef1822cff50fac09c8e6798b1e38a25ba8dbb79a
Willame Damasceno <willamedamasceno@unimedce.com.br>
2023-07-06 15:08:43 -0300

    Métodos

--- vue-project/src/components/Usuario.vue
+++ vue-project/src/components/Usuario.vue
@@ -6,32 +6,34 @@
                 type="text"
                 id="nome" 
                 name="nome" 
-                v-model="pessoa.nome"/>
+                v-model="nome"/>
             <br/>
             <label for="dataNascimento">Data Nascimento:</label>
             <input 
                 type="number" 
                 id="dataNascimento" 
                 name="dataNascimento" 
-                v-model="pessoa.dataNascimento"/>
+                v-model="dataNascimento"/>
             <br/>
         </form>
     </div>
 
-    <p style="text-align: center;">{{ pessoa.nome }} nasceu em {{ pessoa.dataNascimento }}</p>
+    <button class="botao" v-on:click="calcularIdade">Calcula idade</button>
+
+    <p style="text-align: center">{{ nome }} nasceu em {{ dataNascimento }} logo tem {{ idade }}</p>
 </template>
 
-<script setup>
-import { reactive } from "vue";
-//const nome = ref("Willame");
-//const dataNascimento = ref(0);
-//const idade = ref(0);
 
-const pessoa = reactive({
-    nome:'',
-    dataNascimento: 0,
-    idade: 0 
-})
+<script setup>
+import { ref } from "vue";
+const nome = ref("Willame");
+const dataNascimento = ref(0);
+const idade = ref(0);
+
+function calcularIdade(){
+    idade.value = 2023 - dataNascimento.value;
+}
+
 </script>
 
 <style scoped>
@@ -40,5 +42,17 @@ const pessoa = reactive({
     padding: 5px;
     width: 200px;
     background: darkcyan;
+}
+.botao {
+    margin: 5px auto;
+    padding: 5px;
+    display: block;
+    background: darkcyan;
+    border-radius: 5px;
+    border-style: none;
+    cursor: pointer;
+}
+.botao:hover {
+    background: rgb(102, 147, 147);
 }
 </style>
