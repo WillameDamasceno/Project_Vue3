1a7cf93a69a09caffbc9a968ac9ef49a91720574
Willame Damasceno <willamedamasceno@unimedce.com.br>
2023-07-06 10:11:57 -0300

    Iniciando curso de VueJs3

---
+++ vue-project/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+    </div>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+</style>
